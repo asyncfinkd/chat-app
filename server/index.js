9ebe0ec77ee432721d2e5db13341bd6fdb85c84b
asyncfinkd <shamgoingzuck@gmail.com>
2021-12-21 13:52:21 +0400

    server by prettierrc config

--- server/index.js
+++ server/index.js
@@ -1,38 +1,38 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
+const express = require('express')
+const app = express()
+const http = require('http')
+const cors = require('cors')
+const { Server } = require('socket.io')
 
-app.use(cors());
+app.use(cors())
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
   },
-});
+})
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`)
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
+  socket.on('join_room', (data) => {
+    socket.join(data)
 
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
+    console.log(`User with ID: ${socket.id} joined room: ${data}`)
+  })
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit('receive_message', data)
+  })
 
-  socket.on("disconnect", () => {
-    console.log(`User Disconnected ${socket.id}`);
-  });
-});
+  socket.on('disconnect', () => {
+    console.log(`User Disconnected ${socket.id}`)
+  })
+})
 
 server.listen(3001, () => {
-  console.log("Server Running on 3001 PORT");
-});
+  console.log('Server Running on 3001 PORT')
+})
