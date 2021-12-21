1b9ed048a9021a90b4b3ddd712d718878ae67305
asyncfinkd <shamgoingzuck@gmail.com>
2021-12-21 12:38:40 +0400

    server by connection and disconnect

--- server/index.js
+++ server/index.js
@@ -2,10 +2,26 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(cors());
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected ", socket.id);
+  });
+});
 
 server.listen(3001, () => {
   console.log("Server Running on 3001 PORT");
